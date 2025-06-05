994eb7beb59cea9fa78af259a12e353128f929a8
YJoel <joelgarcia6601@gmail.com>
2025-06-04 23:44:36 -0500

    Version Inicial

---
+++ home/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>INPROAIRES</title>
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous"> -->
+  <link rel="stylesheet" href="http://localhost:3000/home/css/styles.css">
+</head>
+
+<body>
+  <div class="content-page">
+    <div class="nav">
+      <div class="logo">
+        <img src="./img/INPROAIRES_LOGO_5.png" alt="">
+      </div>
+      <div class="list-items">
+        <a href="" class="item active">Inicio</a>
+        <a href="" class="item">Nosotros</a>
+        <a href="" class="item">Ventilación</a>
+        <a href="" class="item">Aire Acondicionado</a>
+        <a href="" class="item">Servicios</a>
+        <a href="" class="item">Proyectos</a>
+        <a href="" class="item">Contactanos</a>
+      </div>
+    </div>
+    <div class="section">
+      <div class="background">
+        <img src="./img/fondo-refrigeracion2.png" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <h1>INPROAIRES</h1>
+          <h2>Instalaciones y Proyectos de Aire Acondicionado y Refrigeración</h2>
+        </div>
+        <div class="message">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non animi nobis distinctio, modi consequatur ut
+          voluptatem sequi. Molestias praesentium, voluptatem ea eos placeat nostrum exercitationem nihil cumque commodi
+          quo! Error.
+          Soluta laborum labore fuga quisquam quasi necessitatibus omnis repellat, quis mollitia illum provident
+          perspiciatis commodi nemo! Neque modi itaque dicta est perferendis sequi eaque porro asperiores? Minus
+          dignissimos ab dolorum.
+          Quis commodi rem a accusantium minima? Et, dolores nisi. Provident, fugiat laboriosam. Hic reprehenderit
+          beatae optio omnis, distinctio architecto exercitationem quae pariatur ad provident quaerat, similique ipsa
+          ipsam. Placeat, nobis?
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="background">
+        <img src="./img/fondo-refrigeracion1.png" alt="">
+      </div>
+      <div class="content">
+        <div class="title">
+          <h1>INPROAIRES</h1>
+          <h2>Instalaciones y Proyectos de Aire Acondicionado y Refrigeración</h2>
+        </div>
+        <div class="message">
+          Lorem ipsum dolor sit, amet consectetur adipisicing elit. Non animi nobis distinctio, modi consequatur ut
+          voluptatem sequi. Molestias praesentium, voluptatem ea eos placeat nostrum exercitationem nihil cumque commodi
+          quo! Error.
+          Soluta laborum labore fuga quisquam quasi necessitatibus omnis repellat, quis mollitia illum provident
+          perspiciatis commodi nemo! Neque modi itaque dicta est perferendis sequi eaque porro asperiores? Minus
+          dignissimos ab dolorum.
+          Quis commodi rem a accusantium minima? Et, dolores nisi. Provident, fugiat laboriosam. Hic reprehenderit
+          beatae optio omnis, distinctio architecto exercitationem quae pariatur ad provident quaerat, similique ipsa
+          ipsam. Placeat, nobis?
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <!-- <div class="footer-section">
+        <div class="rounded-img">
+          <img src="./img/INPROAIRES_LOGO_10.png" alt="">
+        </div>
+      </div> -->
+      <div class="footer-section">
+        <h3>
+          Contactanos
+        </h3>
+      </div>
+      <div class="footer-section">
+        <h3>
+          Escríbenos
+        </h3>
+      </div>
+      <div class="footer-section">
+        <h3>
+          Visíta Nuestras instalaciones
+        </h3>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d611.8738804118848!2d-81.71086091648051!3d12.58287886287043!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f05a5e04fa6ea49%3A0x347fd0548c33855e!2sCra.%2013%20%2318%2043%2C%20Br.%20Natania%2C%20San%20Andr%C3%A9s%2C%20San%20Andr%C3%A9s%20y%20Providencia!5e1!3m2!1ses-419!2sco!4v1749091945647!5m2!1ses-419!2sco"
+          width="400" height="300" style="border:0;" allowfullscreen="" loading="lazy"
+          referrerpolicy="no-referrer-when-downgrade"></iframe>
+      </div>
+      <div class="bottom-content"></div>
+      <div class="background">
+        <img src="./img/INPROAIRES_LOGO_10.png" alt="">
+      </div>
+    </div>
+    <div class="redes-sociales left">
+      <div class="item">
+        <img src="./img/facebook-icon.png" alt="">
+      </div>
+      <div class="item">
+        <img src="./img/instagram-icon.png" alt="">
+      </div>
+    </div>
+    <div class="redes-sociales right">
+      <div class="item">
+        <img src="./img/whatsapp-icon.png" alt="">
+      </div>
+    </div>
+  </div>
+
+  <!-- <script src=" https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js"
+    integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous">
+    </script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.min.js"
+    integrity="sha384-RuyvpeZCxMJCqVUGFI0Do1mQrods/hhxYlcVfGPOfQtPJh0JCw12tUAZ/Mv10S7D"
+    crossorigin="anonymous"></script> -->
+</body>
+
+</html>
