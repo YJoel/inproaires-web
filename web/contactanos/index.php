118bf1b84c317b00fb7f4a2e1c8bebeb5da479e3
YJoel <joelgarcia6601@gmail.com>
2025-07-03 10:40:32 -0500

    Cambio de referencias absolutas por referencias relativas

--- web/contactanos/index.php
+++ web/contactanos/index.php
@@ -5,10 +5,10 @@
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1.0" />
   <title>INPROAIRES</title>
-  <link rel="shortcut icon" href="http://localhost:3000/web/img/elice_inproaires_5.png" type="image/x-icon" />
+  <link rel="shortcut icon" href="./../img/elice_inproaires_5.png" type="image/x-icon" />
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous" />
-  <link rel="stylesheet" href="http://localhost:3000/web/css/styles.css" />
+  <link rel="stylesheet" href="./../css/styles.css" />
   <meta name="description"
     content="¿Necesitas una solución en aire acondicionado? Escríbenos y recibe atención técnica especializada para tu hogar, oficina o empresa.">
 </head>
