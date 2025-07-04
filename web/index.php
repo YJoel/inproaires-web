007c7ceaa9e58ac36bce01da302f9dc3fff3eb98
YJoel <joelgarcia6601@gmail.com>
2025-07-04 04:16:33 -0500

    corrección de ruta de acceso a los estilos

--- web/index.php
+++ web/index.php
@@ -8,7 +8,7 @@
   <link rel="shortcut icon" href="./../img/elice_inproaires_5.png" type="image/x-icon">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet"
     integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
-  <link rel="stylesheet" href="./../css/styles.css">
+  <link rel="stylesheet" href="./css/styles.css">
   <meta name="description"
     content="Expertos en instalación, mantenimiento y reparación de sistemas HVAC. Conoce nuestros servicios y la calidad que nos distingue en San Andrés y el resto del país.">
 </head>
